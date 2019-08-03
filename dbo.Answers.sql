2f220432fc7400bd3e08dae21b35d8f705a4a535
maslina3000 <40323707+maslina3000@users.noreply.github.com>
2019-08-03 15:34:48 +0300

    ^

---
+++ dbo.Answers.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Answers] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (MAX) NOT NULL,
+    [Age]           INT            NOT NULL,
+    [Gender]        NVARCHAR (MAX) NOT NULL,
+    [BirthDate]     DATETIME2 (7)  NOT NULL,
+    [MaritalStatus] NVARCHAR (MAX) NOT NULL,
+    [Programming]   NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Answers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
